9b76f46df574b8626cf0735377c9ab34942cb64c
unknown <pcsidsub@D19-serv.upc.edu.pe>
2012-07-10 20:12:52 -0500

    simple form added

--- bookstore/db/schema.rb
+++ bookstore/db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120707182815) do
+ActiveRecord::Schema.define(:version => 20120707184506) do
 
   create_table "books", :force => true do |t|
     t.string   "title"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.text     "description"
+  end
+
+  create_table "user_books", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "book_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "users", :force => true do |t|
