a00205926ef70cf588b7da743b84b9df4ae6a1f9
unknown <pcsidsub@D19-serv.upc.edu.pe>
2012-07-17 22:10:56 -0500

    chat01

---
+++ DEW_chat/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DEWChat::Application.config.secret_token = 'daff75cb75de123d44fe679bea6d6a167743c4adc1bda5ddd954d7083bcdf3fd0e2ec447e3c55a2db1e00b8af1f5f28a3e2d9cf1a976640c1ae540f67643f77c'
